dbb156ff681693c2aa7e935b468348d3bf0d46dc
noaqq <maximbaymetov2002@icloud.com>
2023-01-20 14:14:24 +0300

    Logic and GUI

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ buttons = (
     ("0", ".", "=", "+", "4"),
 )
 
+# active session in calculator
 activeStr = ""
+#list of command
 stack = []
 
 
@@ -19,9 +21,9 @@ def logic():
     global label
     global stack
     res = 0
-    number1 = Decimal(stack.pop())
-    operation = stack.pop
     number2 = Decimal(stack.pop())
+    operation = stack.pop
+    number1 = Decimal(stack.pop())
 
     if operation == "+":
         res = number1 + number2
@@ -31,3 +33,52 @@ def logic():
         res = number1 * number2
     if operation == "/":
         res = number1 / number2
+    label.configure(text=str(res))
+
+
+def click(text):
+    global activeStr
+    global stack
+    if text == "CE":
+        stack.clear()
+        activeStr = ""
+        label.configure(text = "0")
+    elif "0" <=text <="9":
+        activeStr += text
+        label.configure(text=activeStr)
+    elif text == '.':
+        if activeStr.find('.') == -1:
+            activeStr += text
+            label.configure(text=activeStr)
+    else:
+        if len(stack) >= 2:
+            stack.append(label['text'])
+            logic()
+            stack.clear()
+            stack.append(label['text'])
+            activeStr = ''
+            if text != '=':
+                stack.append(text)
+        else:
+            if text != '=':
+                stack.append(label['text'])
+                stack.append(text)
+                activeStr = ''
+                label.configure(text='0')
+
+
+label = Label(root, text='2', width=62, height=6)
+label.grid(row=0, column=0, columnspan=4, sticky="nsew")
+
+button = Button(root, text='CE', command=lambda text='CE': click(text))
+button.grid(row=1, column=3, sticky="nsew")
+for row in range(4):
+    for col in range(4):
+        button = Button(root, text=buttons[row][col],
+            command=lambda row=row, col=col: click(buttons[row][col]))
+        button.grid(row=row + 2, column=col, sticky="nsew")
+
+root.grid_rowconfigure(6, weight=1)
+root.grid_columnconfigure(4, weight=1)
+
+root.mainloop()
